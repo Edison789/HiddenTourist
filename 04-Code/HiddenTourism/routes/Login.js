7acb419a990a2028e620a6ab2390fb938533d7c6
DanielAvila0603 <danielavilac.da@gmail.com>
2021-08-05 17:58:14 -0500

    Update login turist

--- 04-Code/HiddenTourism/routes/Login.js
+++ 04-Code/HiddenTourism/routes/Login.js
@@ -3,24 +3,44 @@ const routes = express.Router();
 
 //login authentication
 
-routes.get('/loged', (req, res) => {
+routes.get('/logedAdmin', (req, res) => {
     res.redirect('/administratorGUI.html');
 });
+
+routes.get('/logedUser',(req,res)=>{
+    res.redirect('/userGUI.html');
+})
 
 routes.post('/login', (req, res) => {
     const user = req.body.user
     const passwrd = req.body.passwrd
-    const query = `SELECT * FROM administrator WHERE USERADMINISTRATOR = ? AND PASSWORDADMINISTRATOR = ? `;
-    req.getConnection((err, conn) => {
-        conn.query(query, [user, passwrd], (err, rows, fields) => {
-            if (err) throw err
-            if (rows.length <= 0) {
-                res.redirect('/login.html')
-            } else {
-                res.redirect('/loged');
-            }
+    const type = req.body.fav_language
+    if (type == 'Administrator') {
+        const query = `SELECT * FROM administrator WHERE USERADMINISTRATOR = ? AND PASSWORDADMINISTRATOR = ? `;
+        req.getConnection((err, conn) => {
+            conn.query(query, [user, passwrd], (err, rows, fields) => {
+                if (err) throw err
+                if (rows.length <= 0) {
+                    res.redirect('/login.html')
+                } else {
+                    res.redirect('/logedAdmin');
+                }
+            })
         })
-    })
+    } else {
+        const query = `SELECT * FROM tourist WHERE USERTOURIST = ? AND PASSWORDTOURIST = ? `;
+        req.getConnection((err, conn) => {
+            conn.query(query, [user, passwrd], (err, rows, fields) => {
+                if (err) throw err
+                if (rows.length <= 0) {
+                    res.redirect('/login.html')
+                } else {
+                    res.redirect('/logedUser');
+                }
+            })
+        })
+    }
+
 })
 
 module.exports = routes;
