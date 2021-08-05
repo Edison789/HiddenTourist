a002eb8f7a108f253f393792461dbfe21dfc209a
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2021-08-05 17:47:52 -0500

    Update InsertesSQL

--- 06-SQL/InsertsSQL.sql
+++ 06-SQL/InsertsSQL.sql
@@ -18,3 +18,24 @@ INSERT INTO `hiddentourismnode`.`administrator` (`IDADMINISTRATOR`, `FIRSTNAMEAD
 VALUES ('1745236985', 'Teresa', 'Guerra', 'goma874', 'tere412', 'tere25@gmail.com');
 INSERT INTO `hiddentourismnode`.`administrator` (`IDADMINISTRATOR`, `FIRSTNAMEADMINISTRATOR`, `LASTNAMEADMINISTRATOR`, `PASSWORDADMINISTRATOR`, `USERADMINISTRATOR`, `EMAILADMINISTRATOR`) 
 VALUES ('1798563201', 'Julio', 'Jaramillo', 'cinta887', 'julio12', 'julio@outlook.com');
+
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('AB12', 'Panecillo', 'Pichincha', 'Quito', 'Centro de Quito', '0996587412', 'Comer');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('CD34', 'Casa de los Venados', 'Pichincha', 'Cayambe', 'Av Panamericana 0E35', '0958741254', 'Interactuar con animales');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('ED25', 'Cascada de Manuel', 'El Oro', 'Machala', 'El Esquinero', '0963652189', 'Nadar,Caminar y comer');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('QW85', 'Catedral de Machala', 'El Oro', 'Machala', 'Plaza de armas central', '0955487410', 'Caminar, comer y fotografias');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('VF85', 'Cerro Santa Ana', 'Guayas', 'Guayaquil', 'Av. Rio Esmeraldas', '0935252148', 'Caminar y comer');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('HJ47', 'Parque Ayora', 'Carchi', 'Tulcan', 'Av. Manabi 63', '0995118125', 'Caminar, comer y comprar');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('JU69', 'Parque Recreacional Jipiro', 'Loja', 'Loja', 'Velasco Ibarra 1982', '0995142584', 'Caminar, Jugar y comer');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('TY87', 'Piramides Cochasqui', 'Pichincha', 'Pedro Moncayo', 'Cochasqui', '0885522147', 'Caminar, comer y fotografias');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('BG63', 'Puerta de la Ciudad', 'Loja', 'Loja', '18 de Noviembre', '0996362547', 'Caminar, Recorrer y Comprar');
+INSERT INTO `hiddentourismnode`.`place` (`IDPLACE`, `NAMEPLACE`, `PROVINCEPLACE`, `CANTONPLACE`, `ADDRESSPLACE`, `CONTACTPLACE`, `ACTIVITIESPLACE`) 
+VALUES ('LO20', 'Tulipe', 'Pichincha', 'Quito', 'Nanegalito', '0995632147', 'Nadar,Caminar y comer');
